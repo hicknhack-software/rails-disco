e767b131f99b7fb8a4c6b6574144070a08fd0663
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2013-10-05 02:15:24 +0200

    fixed failing test

--- active_domain/spec/lib/command_processor_spec.rb
+++ active_domain/spec/lib/command_processor_spec.rb
@@ -2,6 +2,7 @@ require_relative '../spec_helper'
 
 describe ActiveDomain::CommandProcessor do
   class TestDummy
+    include ActiveEvent::EventType
   end
   class TestDummyProcessor
     include ActiveDomain::CommandProcessor
@@ -19,7 +20,7 @@ describe ActiveDomain::CommandProcessor do
 
   describe 'instance' do
     it 'allows event method' do
-      magic_number = 42
+      magic_number = TestDummy.new
       expect(ActiveDomain::ProjectionRegistry).to receive(:invoke).with(magic_number)
       expect(ActiveDomain::EventRepository).to receive(:store).with(magic_number)
       expect(ActiveEvent::EventServer).to receive(:publish).with(magic_number)
