d9f36db6aa25f0ea828f4143fdd3af04395c2a1d
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2014-04-22 19:19:24 +0200

    fixed tests

--- active_projection/spec/lib/projection_type_registry_spec.rb
+++ active_projection/spec/lib/projection_type_registry_spec.rb
@@ -13,16 +13,7 @@ describe ActiveProjection::ProjectionTypeRegistry do
     ActiveProjection::ProjectionTypeRegistry.instance.should be
   end
 
-  it 'invokes event handlers' do
-    headers = {}
-    event = TestEvent.new
-    expect_any_instance_of(TestProjection).to receive(:evaluate).with(headers).and_return(true)
-    expect_any_instance_of(TestProjection).to receive(:invoke).with(event, headers)
-    ActiveProjection::ProjectionTypeRegistry.process(headers, event)
-  end
-
-  it 'synchronizes the known projections with the db' do
-    expect(ActiveProjection::ProjectionRepository).to receive(:ensure_exists).with('TestProjection')
-    ActiveProjection::ProjectionTypeRegistry.sync_projections
+  it 'report projections' do
+    ActiveProjection::ProjectionTypeRegistry.projections.count.should eq 1
   end
 end
